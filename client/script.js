c4de5859634f884494028546e88242d7faa0ee6a
Rohit <rohitkhatri111112@gmail.com>
2024-03-14 16:52:11 +0530

    Server and .env updated

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
 
   // fetch data from server
 
-  const response = await fetch("https://openai-chatgpt-3-5-turbo-api.onrender.com", {
+  const response = await fetch("http://localhost:8000", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
